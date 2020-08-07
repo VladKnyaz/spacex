b46a0d231691928246f4bb020bbcf90c06e9ff41
VladKnyaz <69315057+VladKnyaz@users.noreply.github.com>
2020-08-07 14:58:20 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//			VidBg
+var instance = new vidbg('.video', {
+  mp4: 'video/world.mp4', // URL or relative path to MP4 video
+  webm: 'video/world.webm', // URL or relative path to webm video
+  poster: 'img/poster.jpg', // URL or relative path to fallback image
+  overlay: false, // Boolean to display the overlay or not
+});
+//			Rellax
+var rellax = new Rellax('.rocket');
+
+//			JS Code
+if(document.body.clientWidth < 576) {
+	rellax.destroy();
+}
+
+AOS.init();
